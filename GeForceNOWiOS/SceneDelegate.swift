252d5187dcda63fcdd2b13eee6c11fcc584846c5
hamalainenmiska100-hue <hamalainen.miska100@gmail.com>
2026-04-29 17:02:18 +0300

    Fix over-aggressive external navigation and OAuth return handling

--- GeForceNOWiOS/SceneDelegate.swift
+++ GeForceNOWiOS/SceneDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
+    private var webViewController: WebViewController?
+
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
@@ -11,8 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = WebViewController()
+        let webViewController = WebViewController()
+        window.rootViewController = webViewController
         window.makeKeyAndVisible()
         self.window = window
+        self.webViewController = webViewController
+    }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else { return }
+        webViewController?.handleExternalReturn(url: url)
     }
 }
